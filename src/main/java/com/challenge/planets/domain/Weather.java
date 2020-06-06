bbc0822ec763f329d464ebd55fd2feadf56480c9
Sergio Cano <serdeliverance@gmail.com>
2020-06-06 20:35:11 -0300

    h2 and jpa repositories

--- src/main/java/com/challenge/planets/domain/Weather.java
+++ src/main/java/com/challenge/planets/domain/Weather.java
@@ -2,10 +2,26 @@ package com.challenge.planets.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Weather {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private int day;
+
+    @Enumerated(EnumType.STRING)
     private WeatherEnum weather;
+
+    public static Weather of(int day, WeatherEnum weatherEnum) {
+        return new Weather(null, day, weatherEnum);
+    }
 }
--- src/main/java/com/challenge/planets/repository/WeatherRepository.java
+++ src/main/java/com/challenge/planets/repository/WeatherRepository.java
@@ -1,12 +1,9 @@
 package com.challenge.planets.repository;
 
 import com.challenge.planets.domain.Weather;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class WeatherRepository {
-    // TODO implement
-    public void save(Weather c) {
-        // TODO implement
-    }
+public interface WeatherRepository extends CrudRepository<Weather, Long> {
 }
